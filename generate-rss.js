eb1ce285ba67ffb382c082f75e4f5e7c930ac6b9
miladsoft <miladsoft@users.noreply.github.com>
2024-05-21 12:16:03 +0000

    deploy: a2145fb86f6b0b7353ddd9d86dba0d4a11cd24bf

--- generate-rss.js
+++ generate-rss.js
@@ -5,19 +5,35 @@ import path from 'path';
 const apiUrl = 'https://api.digikaladiscounts.com/items-history.json';
 const rssPath = path.join(process.cwd(), 'rss.xml');
 
+// Function to encode URL to Base64 using UTF-8 encoding
+function encodeToBase64(url) {
+    return Buffer.from(url).toString('base64');
+}
+
+// Function to generate affiliate link
+function generateAffiliateLink(digikalaUrl) {
+    const baseUrl = digikalaUrl.split('?')[0]; // Remove query parameters if any
+    const base64Url = encodeToBase64(baseUrl);
+    const affiliateLink = `https://dgkl.io/api/v1/Click/b/rGACB?b64=${base64Url}`;
+    return affiliateLink;
+}
+
 async function generateRSS() {
     try {
         const response = await fetch(apiUrl);
         const items = await response.json();
 
-        const rssItems = items.map(item => `
-            <item>
-                <title>${item.Title}</title>
-                <link>https://digikala.com${item.Url}</link>
-                <description>${item.Title} با ${item.DiscountPercent}% تخفیف.</description>
-                <pubDate>${new Date(item.Date).toUTCString()}</pubDate>
-            </item>
-        `).join('');
+        const rssItems = items.map(item => {
+            const affiliateLink = generateAffiliateLink(`https://www.digikala.com/product/${item.Url.split('/')[2]}/`);
+            return `
+                <item>
+                    <title>${item.Title}</title>
+                    <link>${affiliateLink}</link>
+                    <description>${item.Title} با ${item.DiscountPercent}% تخفیف.</description>
+                    <pubDate>${new Date(item.Date).toUTCString()}</pubDate>
+                </item>
+            `;
+        }).join('');
 
         const rssContent = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
